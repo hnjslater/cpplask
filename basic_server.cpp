56fe14596d1310fe76176057ecdc7cac44c827c4
hnjslater <henry.slater@gmail.com>
2015-09-07 00:27:13 +0100

    basic server now handles multiple concurrent requests but responds one at a time

--- basic_server.cpp
+++ basic_server.cpp
@@ -20,6 +20,7 @@
 #include <sstream>
 #include <atomic>
 #include <thread>
+#include <algorithm>
 
 namespace cpplask {
 
@@ -83,23 +84,20 @@ struct listen_socket {
     }
 };
 
-void serve(cpplask::service_t& service, int listen_socket_fd, sigset_t orig);
+void serve(cpplask::service_t& service, int listen_socket_fd);
 void basic_serve(cpplask::service_t& service, uint32_t port) {
     g_keep_going=true;
     listen_socket listen_socket(port);
     std::cerr << "listening on port " << port << std::endl;
     auto orig = stop_signals();
-    serve(service, listen_socket.listen_socket_fd, orig);
+    serve(service, listen_socket.listen_socket_fd);
     restore_signals(orig);
     std::cerr << "Exiting" << std::endl;
 }
 
-auto parse_request(int socket_fd) -> auto {
-    char buffer[2048] = {0};
-    int num_read = read(socket_fd,buffer,2040);
-    if (num_read <= 0) {
-        throw std::runtime_error("ERROR reading from socket");
-    }
+auto parse_request(std::string bufferstr) -> auto {
+    char* buffer = const_cast<char*>(bufferstr.c_str());
+
     char* strtok_state;
     char* current_line_buf = 0;
 
@@ -124,7 +122,7 @@ auto parse_request(int socket_fd) -> auto {
     return std::make_pair(path, headers); 
 }
 
-void serve(cpplask::service_t& service, int listen_socket_fd, sigset_t ) {
+void serve(cpplask::service_t& service, int listen_socket_fd) {
 
     struct sigaction new_action;
     sigemptyset (&new_action.sa_mask);
@@ -134,24 +132,34 @@ void serve(cpplask::service_t& service, int listen_socket_fd, sigset_t ) {
     sigaction(SIGQUIT, &new_action, NULL);
     sigaction(SIGTERM, &new_action, NULL);
 
+    std::vector<std::pair<int,std::string>> incoming_fds;
+
     while (g_keep_going) {
-        std::cerr << "listening" << std::endl;
         listen(listen_socket_fd,5);
 
         fd_set rfds;
-
         FD_ZERO(&rfds);
         FD_SET(listen_socket_fd, &rfds);
+        int max_fd = listen_socket_fd;
+        for (auto&& fdpair : incoming_fds) {
+            int fd = std::get<0>(fdpair);
+            max_fd = std::max(max_fd, fd);
+            FD_SET(fd, &rfds);
+        }
 
         sigset_t mask;
         sigemptyset (&mask);
 
-        int pselect_rtn = pselect(listen_socket_fd+1, &rfds, NULL, NULL, NULL, &mask);
+        int pselect_rtn = pselect(max_fd+1, &rfds, NULL, NULL, NULL, &mask);
+        if (pselect_rtn < 0) {
+            perror("pselect");
+        }
         if (!g_keep_going) {
             break;
         }
-        else if (pselect_rtn > 0 && FD_ISSET(listen_socket_fd, &rfds)) {
-            
+        if (pselect_rtn > 0 && FD_ISSET(listen_socket_fd, &rfds)) {
+            // new client connecting    
+            std::cerr << "client connected" << std::endl;
             sockaddr_in client_address;
             socklen_t client_address_len = sizeof(client_address);
 
@@ -161,31 +169,52 @@ void serve(cpplask::service_t& service, int listen_socket_fd, sigset_t ) {
             if (accept_socket_fd < 0) {
                 throw std::runtime_error("Error during accept");
             }
-            pselect_rtn = pselect(accept_socket_fd+1, &rfds, NULL, NULL, NULL, &mask);
-            if (!g_keep_going) {
-                close(accept_socket_fd);
-                break;
-            }
-
-            auto parameters = parse_request(accept_socket_fd);
-            cpplask::request_t req(std::get<0>(parameters), std::get<1>(parameters));
-            service.serve(req);
-
-            std::string message = req.response().str();
-
-            std::stringstream ss;
-            ss << "HTTP/1.1 " << req.response().code() << " " << req.response().status() << "\n";
-            ss << "Content-Type: text/plain\n";
-            ss << "Content-length: " << message.size() << "\n\n";
-            ss << message;
-            int num_written = write(accept_socket_fd,ss.str().c_str(),ss.str().size());
-            if (num_written < 0) {
-                throw std::runtime_error("Error during writing to socket");
-            }
-            close(accept_socket_fd);
+            incoming_fds.emplace_back(accept_socket_fd, std::string());
         }
-    }
 
+
+        incoming_fds.erase(std::remove_if(incoming_fds.begin(), incoming_fds.end(), [&](auto&& fdpair) {
+            int& accept_socket_fd = std::get<0>(fdpair);
+            std::string& buffstr = std::get<1>(fdpair);
+            
+            if (FD_ISSET(accept_socket_fd, &rfds)) {
+
+                char buffer[2048] = {0};
+                int num_read = read(accept_socket_fd,buffer,2040);
+                if (num_read <= 0) {
+                    throw std::runtime_error("ERROR reading from socket");
+                }
+
+                buffstr += std::string(buffer, num_read);
+                std::string marker = "\r\n\r\n";
+
+                if (std::search(buffstr.begin(), buffstr.end(), marker.begin(), marker.end()) == buffstr.end()) {
+                    return false;
+                }
+
+                auto parameters = parse_request(buffstr);
+                cpplask::request_t req(std::get<0>(parameters), std::get<1>(parameters));
+                service.serve(req);
+
+                std::string message = req.response().str();
+
+                std::stringstream ss;
+                ss << "HTTP/1.1 " << req.response().code() << " " << req.response().status() << "\n";
+                ss << "Content-Type: text/plain\n";
+                ss << "Content-length: " << message.size() << "\n\n";
+                ss << message;
+                int num_written = write(accept_socket_fd,ss.str().c_str(),ss.str().size());
+                if (num_written < 0) {
+                    throw std::runtime_error("Error during writing to socket");
+                }
+                close(accept_socket_fd);
+                return true;
+            }
+            else {
+                return false;
+            }
+        }), incoming_fds.end());
+    }
 } 
 
 }
